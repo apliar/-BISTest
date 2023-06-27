cfa699230362013803b4363cb356c00fd4e23dba
apliar <75115989+apliar@users.noreply.github.com>
2023-06-27 13:42:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using BISTest;
+
+Singleton singleton = Singleton.GetInstance();
+
+(new Thread(() =>
+{
+    List<Task<int>> tasks = new();
+    var task1 = Task.Run(() => singleton.DoCalculation(2));
+    tasks.Add(task1);
+    var task2 = Task.Run(() => singleton.DoCalculation(3));
+    tasks.Add(task2);
+    foreach (var task in tasks)
+    {
+        Console.WriteLine(task.Status);
+    }
+    foreach (var task in tasks)
+    {
+        try
+        {
+            Console.WriteLine(task.Result);
+        }
+        catch (AggregateException ae)
+        {
+            foreach (var ex in ae.InnerExceptions)
+            {
+                if (ex is ArgumentException)
+                    Console.WriteLine(ex.Message);
+                else
+                    throw ex;
+            }
+        }
+    }
+})).Start();
+
+(new Thread(() =>
+{
+    List<Task<int>> tasks = new();
+    var task1 = Task.Run(() => singleton.DoCalculation(46340));
+    tasks.Add(task1);
+    var task2 = Task.Run(() => singleton.DoCalculation(46341));
+    tasks.Add(task2);
+    foreach (var task in tasks)
+    {
+        Console.WriteLine(task.Status);
+    }
+    foreach (var task in tasks)
+    {
+        try
+        {
+            Console.WriteLine(task.Result);
+        }
+        catch (AggregateException ae)
+        {
+            foreach (var ex in ae.InnerExceptions)
+            {
+                if (ex is ArgumentException)
+                    Console.WriteLine(ex.Message);
+                else
+                    throw ex;
+            }
+        }
+    }
+})).Start();
---
+++ Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BISTest
+{
+    internal class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy =
+    new Lazy<Singleton>(() => new Singleton());
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            return lazy.Value;
+        }
+
+        public int DoCalculation(int value)
+        {
+            if (value >= Convert.ToInt32(Math.Sqrt(int.MaxValue)))
+                throw new ArgumentException($"Value is too large: {value}/{Convert.ToInt32(Math.Sqrt(int.MaxValue))-1}");
+            System.Threading.Thread.Sleep(1000);
+            return Convert.ToInt32(Math.Pow(value, 2));
+        }
+    }
+}
